4045642566d29fbfc1daf4085ae67e74c3bd9a12
Rodrigo Henrique Moreira <ct0036@blanver.com.br>
2021-04-20 10:40:38 -0300

    index, style & script changed

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function Enviar() {
     var dados1 = document.getElementById("dados1");
     var dados2 = document.getElementById("dados2");
     var dados3 = document.getElementById("dados3");
+    var dados4 = document.getElementById("dados4");
 
   
-    if (dados1.value != "" && dados2.value != "" && dados3.value != "") {
+    if (dados1.value != "" && dados2.value != "" && dados3.value != "" && dados4.value != "") {
         alert('Obrigado sr.(a) ' + dados2.value + ', os seus dados foram encaminhados com sucesso!');
     }else{
       alert('Atenção sr.(a), os campos marcados com asterísco (*) são obrigatórios!')
